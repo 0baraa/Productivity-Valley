7f2858b8f8f3bb75bed1a377c5087da60da7fad6
s27124jh <jianan.hou-2@student.manchester.ac.uk>
2024-03-13 20:11:06 +0000

    1

---
+++ djangoProject/djangoProject/authentication/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.shortcuts import render
+import uuid
+from django.shortcuts import redirect
+from django.urls import reverse
+import requests
+
+
+def cas_callback(request):
+    csticket = request.GET.get('csticket')
+    username = request.GET.get('username')
+    fullname = request.GET.get('fullname')
+
+#     validation_url = f"http://studentnet.cs.manchester.ac.uk/authenticate/?url={request.build_absolute_uri()}&csticket={csticket}&version=3&command=confirm&username={username}&fullname={fullname}"
+    validation_url = f"https://login.manchester.ac.uk/cas//?url={request.build_absolute_uri()}&csticket={csticket}&version=3&command=confirm&username={username}&fullname={fullname}"
+    response = requests.get(validation_url)
+
+    if response.text == 'true':
+        request.session['username'] = username
+
+        request.session['fullname'] = fullname
+        # 认证成功，重定向到受保护的主页
+        return redirect('protected_home')
+    else:
+        # 认证失败，重定向到错误页面或主页，并附带错误消息
+        return redirect(f"{reverse('home')}?error=Authentication+Failed")
+
+
+
+def redirect_to_auth_service(request):
+    csticket = generate_csticket(request)  # 调用上面的函数生成 CSTICKET
+#     auth_service_url = f"http://studentnet.cs.manchester.ac.uk/authenticate/?url={request.build_absolute_uri(reverse('cas_callback'))}&csticket={csticket}&version=3&command=validate"
+    auth_service_url = f"https://login.manchester.ac.uk/cas/?url={request.build_absolute_uri(reverse('cas_callback'))}&csticket={csticket}&version=3&command=validate"
+    return redirect(auth_service_url)
+
+
+def generate_csticket(request):
+    csticket = str(uuid.uuid4())  # 生成随机的 CSTICKET
+    request.session['csticket'] = csticket  # 保存到会话中
+    return csticket
+
+
+def home(request):
+    return render(request, 'authentication/landing.html')
+
+
+@login_required
+def protected_home(request):
+    return render(request, 'authentication/index.html')
+
+
+def signout(request):
+    return redirect('cas_ng_logout')
+
+
+def game_view(request):
+    return render(request, 'authentication/game_index.html')
+
+
+def chart_view(request):
+    data = {
+        'labels': ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        'data': [12, 19, 3, 5, 2, 3],
+    }
+    return render(request, 'chart.html', {'chart_data': data})
---
+++ djangoProject/djangoProject/djangoProject/settings.py
@@ -0,0 +1,114 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = 'django-insecure-&(o1s5+xm&vhhk0@^*4s+mkk54g^y4r*6esg2*r-!(9p%8j%3m'
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_cas_ng',
+    'PVdb',
+    'rest_framework',
+    'corsheaders',
+    'DB',
+    ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_cas_ng.middleware.CASMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+ROOT_URLCONF = 'djangoProject.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            "templates",
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'djangoProject.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_URL = '/static/'
+
+# CAS settings
+CAS_SERVER_URL = 'https://login.manchester.ac.uk/cas/'
+# CAS_SERVER_URL = 'https://www.manchester.ac.uk/'
+# Where to redirect after successful login
+CAS_REDIRECT_URL = '/'
