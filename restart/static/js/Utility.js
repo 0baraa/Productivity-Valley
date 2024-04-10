d92bd43319e2c8b15f4e797033f8c243d2c8ed25
s27124jh <jianan.hou-2@student.manchester.ac.uk>
2024-04-10 21:00:02 +0100

    .

--- restart/static/js/Utility.js
+++ restart/static/js/Utility.js
@@ -4,6 +4,8 @@
 export default class Utility {
     static editMode = false;
     static deleteMode = false;
+    static working = false;
+    static isPlotReady = false;
 
     static toggleEditMode() {
         this.editMode = !this.editMode;
@@ -51,33 +53,52 @@ export default class Utility {
         let dialogContainer;
         let dialog;
 
-        if(menu === "taskMenu") {
-            dialogContainer = document.querySelector('.menu-container.task-menu');
-            dialog = document.querySelector('.menu.task-menu');
+        switch(menu) {
+            case "taskMenu":
+                dialogContainer = document.querySelector('.menu-container.task-menu');
+                dialog = document.querySelector('.menu.task-menu');
+                break;
+            case "taskSettings":
+                dialogContainer = document.querySelector('.menu-container.task-menu');
+                dialog = document.querySelector('.menu.task-menu');
+                break;
+            case "cropShop":
+                dialogContainer = document.querySelector('.menu-container.shop-menu');
+                dialog = document.querySelector('.menu.shop-menu');
+                break;
+            case "furnitureMenu":
+                dialogContainer = document.querySelector('.menu-container.furniture-menu');
+                dialog = document.querySelector('.menu.furniture-menu');
+                break;
+            case "decorationPlotMenu":
+                dialogContainer = document.querySelector('.menu-container.decoration-plot-menu');
+                dialog = document.querySelector('.menu.decoration-plot-menu');
+                break;
+            case "furnitureShopMenu":
+                dialogContainer = document.querySelector('.menu-container.furniture-shop-menu');
+                dialog = document.querySelector('.menu.furniture-shop-menu');
+                break;
+            case "decorationShopMenu":
+                dialogContainer = document.querySelector('.menu-container.decoration-shop-menu');
+                dialog = document.querySelector('.menu.decoration-shop-menu');
+                break;
+            case "plotShopMenu":
+                dialogContainer = document.querySelector('.menu-container.plots-shop-menu');
+                dialog = document.querySelector('.menu.plots-shop-menu');
+                break;
+            case "settingsMenu":
+                dialogContainer = document.querySelector('.menu-container.settings-menu');
+                dialog = document.querySelector('.menu.settings-menu');
+                break;
+            case "homeMenu":
+                dialogContainer = document.querySelector('.menu-container.home-menu');
+                dialog = document.querySelector('.menu.home-menu');
+                break;
+            case "window":
+                dialogContainer = document.querySelector('.menu-container.chart-menu');
+                dialog = document.querySelector('.menu.chart-menu');
+                break;
         }
-        else if (menu === "cropShop") {
-            dialogContainer = document.querySelector('.menu-container.shop-menu');
-            dialog = document.querySelector('.menu.shop-menu');
-        }
-        else if (menu === "furnitureMenu") {
-            dialogContainer = document.querySelector('.menu-container.furniture-menu');
-            dialog = document.querySelector('.menu.furniture-menu');
-        }
-        else if(menu === "decorationPlotMenu") {
-            dialogContainer = document.querySelector('.menu-container.decoration-plot-menu');
-            dialog = document.querySelector('.menu.decoration-plot-menu');
-        }
-        else if(menu === "furnitureShopMenu") {
-            dialogContainer = document.querySelector('.menu-container.furniture-shop-menu');
-            dialog = document.querySelector('.menu.furniture-shop-menu');
-        }
-
-        else if(menu === "window"){
-            dialogContainer = document.querySelector('.menu-container.chart-menu');
-            dialog = document.querySelector('.menu.chart-menu');
-        }
-
-    
 
         
         if (dialog.open) {
@@ -94,20 +115,42 @@ export default class Utility {
 
             scene.sys.game.input.enabled = false;
         }
-        // if (dialog) { // 确保 dialog 不是 undefined 或 null
-        //     if (dialog.open) { // 这里假设 dialog 是一个 <dialog> 元素
-        //         dialogContainer.style.display = 'none';
-        //         dialog.close();
-        //         scene.sys.game.input.enabled = true;
-        //     } else {
-        //         dialogContainer.style.display = 'block';
-        //         dialog.showModal(); // 显示 <dialog> 元素
-        //         scene.sys.game.input.enabled = false;
-        //     }
-        // } else {
-        //     console.error('Dialog element not found for menu:', menu);
-        // }
+    }
 
+    static throwConfirmationScreen (scene, action, prompt, promptNote) {
+        let dialogContainer = document.querySelector('.menu-container.confirmation');
+        let dialog = document.querySelector('.menu.confirmation');
+        let acceptButton = document.getElementById('accept-confirmation');
+        let denyButton = document.getElementById('deny-confirmation');
+        let messagePara = document.getElementById('confirmation-message');
+        let notePara = document.getElementById('confirmation-note');
+        const closeWindow = (event) => {
+            acceptButton.removeEventListener('click', closeWindow);
+            denyButton.removeEventListener('click', closeWindow);
+            dialogContainer.style.display = 'none';
+            dialog.close();
+            scene.sys.game.input.enabled = true;
+            if (event.srcElement.id == "accept-confirmation") {
+                if (action == "harvestCrops") {
+                    scene.events.emit(action);
+                }
+                else if (action = "deleteAccount") {
+                    //this.deleteAccount();
+                    this.chuckUserOut();
+                }
+            }
+        }
+        messagePara.innerHTML = prompt;
+        notePara.innerHTML = promptNote;
+        dialogContainer.style.display = 'block';
+        dialog.showModal();
+        scene.sys.game.input.enabled = false;
+        acceptButton.addEventListener('click', closeWindow);
+        denyButton.addEventListener('click', closeWindow);
+    }
+
+    static chuckUserOut() {
+        window.location.href = "../landing.html";
     }
 
     static getUserData() {
@@ -125,14 +168,19 @@ export default class Utility {
                 {crop:"sunflower", count: 1},
                 {crop:"carrot", count: 2},
                 {crop:"pumpkin", count: -1},
-                {crop:"flower", count: -1}
+                {crop:"tulip", count: -1}
             ],
-            //plots should have coordinates saved also
+
             "plots": [
-              {"id": 1, "crop": "sunflower", "growthStage": 3, "task": "Maths Homework", "x": 176, "y": 616, "placed": true},
-              {"id": 2, "crop": "sunflower", "growthStage": 9, "task": "Computation Catchup", "x": 272, "y": 616, "placed": true},
-              {"id": 3, "crop": "carrot", "growthStage": 2, "x": 368, "y": 616, "placed": true},
-              {"id": 4, "crop": "nothing", "growthStage": 0, "x": 464, "y": 616, "placed": true},
+              {"plotId": 0, "crop": "sunflower", "growthStage": 10, "growthStep": 12, "x": 176, "y": 616, "placed": true},
+              {"plotId": 1, "crop": "tulip", "growthStage": 9, "growthStep": 12, "x": 272, "y": 616, "placed": true},
+              {"plotId": 2, "crop": "carrot",  "growthStage": 1, "growthStep": 2, "x": 368, "y": 616, "placed": true},
+              {"plotId": 3, "crop": "nothing", "growthStage": 0, "growthStep": 0, "x": 464, "y": 616, "placed": true},
+            ],
+            "tasks": [
+                {"plotId": 0, "taskName": "Maths Homework", "pomodoros": 3, "completed": true, "subtasks": ["week 7", "week 8"]},
+                {"plotId": 1, "taskName": "Operating Systems CW", "pomodoros": 3, "completed": true, "subtasks": ["download files", "start work", "catch up on notes bruh"], "subtasksCompleted": [true,false,false]},
+                {"plotId": 2, "taskName": "go to bed", "pomodoros": 1, "completed": false, "subtasks": ["tidy desk", "brush teeth", "get changed", "go piss girl"], "subtasksCompleted": [true,false,false,false]},
             ],
             "furniture": [
               {"type": "carpet1", "x": 320, "y": 612, "placed": true},
@@ -144,11 +192,10 @@ export default class Utility {
             "decorations": [
                 {"type": "snowman", "x":50, "y":700, "placed": true}
             ],
-            "farmhouse":[
+            "farmhouse":
                 {"level": 1, "x": 70, "y": 570}
-            ]
           }
-    
+
           return this.data;
     }
 
@@ -179,6 +226,10 @@ export default class Utility {
 
     }
 
+    static updateTaskProgress(plotID, step) {
+
+    }
+
     static getPlotData() {
         //get plot data specifically
     }
@@ -204,5 +255,19 @@ export default class Utility {
         }
         this.data.coins -= price;
         console.log(this.data.coins);
+    }
+
+    static getWorkingState() {
+        return this.working;
+    }
+    static setWorkingState(state) {
+        this.working = state;
+    }
+
+    static plotReady() {
+        return this.isPlotReady;
+    }
+    static setPlotReady(state) {
+        this.isPlotReady = state;
     }
 }
